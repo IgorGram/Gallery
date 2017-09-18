096d11409783a1efcfa499293e424e39540186a8
Igor <grammatchikov@inbox.ru>
2017-09-18 19:25:06 +0300

    конечный вариант

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 "use strict";
 
-let thumbs = document.querySelector(`#thumbs`);
-thumbs.addEventListener(`click`, _onClick);
+var largeImg = document.getElementById("largeImg");
 
-function _onClick(event) {
-    let target = event.target;
-    if(target.nodeName !== "IMG")return;
-    alert("aaa");
-    event.preventDefault()
+var thumbs = document.getElementById("thumbs");
+
+thumbs.onclick = function (e) {
+    var target = e.target;
+
+    while (target != this) {
+
+        if (target.nodeName == "A") {
+            showThumbnail(target.href, target.title);
+            return false;
+        }
+
+        target = target.parentNode;
+    }
+
+};
+
+function showThumbnail(href, title) {
+    largeImg.src = href;
+    largeImg.alt = title;
+}
 
 
+/* предзагрузка */
+var imgs = thumbs.getElementsByTagName("img");
+for (var i = 0; i < imgs.length; i++) {
+    var url = imgs[i].parentNode.href;
+    var img = document.createElement("img");
+    img.src = url;
 }
